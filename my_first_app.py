41fbe1889003ce6472a328a2c05e5abe7bb0d197
Jorge Metri <metri988@gmail.com>
2025-05-17 19:00:22 -0600

    first commit with an example of an app in a script

---
+++ my_first_app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 17 17:20:06 2025
+
+@author: metri
+"""
+
+
+import streamlit as st
+
+st.title('My first app!!')
+st.header('This is the header')
+st.write("""
+         # Esta es mi primera app en Streamlit
+         """)
+         
